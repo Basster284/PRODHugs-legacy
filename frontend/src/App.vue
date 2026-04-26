951d70fa090e7b71e5a1720a62b40b0f327ccfbb
misshanya <mishanyadev@gmail.com>
2026-04-26 16:11:15 +0300

    chore(frontend): remove menu button separator

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,7 +3,6 @@ import { RouterView, useRoute } from 'vue-router'
 import { computed } from 'vue'
 import { useAuthStore } from '@/stores/auth'
 import { SidebarProvider, SidebarInset, SidebarTrigger } from '@/components/ui/sidebar'
-import { Separator } from '@/components/ui/separator'
 import { Toaster } from '@/components/ui/sonner'
 import AppSidebar from '@/components/AppSidebar.vue'
 import AppHeader from '@/components/AppHeader.vue'
@@ -23,7 +22,6 @@ const showLayout = computed(() => {
       <SidebarInset>
         <header class="flex h-14 shrink-0 items-center gap-2 border-b px-4">
           <SidebarTrigger class="-ml-1" />
-          <Separator orientation="vertical" class="mr-2 !h-4" />
           <AppHeader />
         </header>
         <main class="flex-1 p-3 sm:p-6">
